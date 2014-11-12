dc3a7146177aae5a6a2693985bb8e6b46646959c
Demitrious Kelly <apokalyptik@apokalyptik.com>
2014-11-12 15:06:17 -0800

    godoc

--- nonce.go
+++ nonce.go
@@ -1,3 +1,26 @@
+// nonce provides you with a small set of tools for working with an in-memory
+// nonce store.  Typically you would create a nonce in your application to help
+// validate the authenticity of requested actions as well as prevent certain
+// kinds of attacks, such as reply attacks.
+//
+// An example use case would be for a web app which allows users to interract
+// with widgets.  In this case we want to create a nonce which we can later use
+// to validate that user ID 123 does, in fact, want to delete widget "foo"
+//
+//    store, _ := nonce.New()
+//    nonce := store.Nonce("123:foo:delete")
+//    // ...
+//    if store.Verify(nonce, "123:foo:delete") {
+//        // delete the widget
+//    } else {
+//        // the nonce has expired,
+//        // or the action has already occured,
+//        // or the user was being tricked into doing dsomething gainst their
+//        //     wishes such as a CSRF attack
+//        // or the user is poking their nose around your API and trying to
+//        //     figure our how your app works.  Sometimes the worst case is
+//        //     just paranoia :)
+//    }
 package nonce
 
 import (
@@ -10,24 +33,40 @@ import (
 	"time"
 )
 
+// Store provides a non-persisted in-memory store for, as well as functions to
+// create and verify, nonces.
 type Store struct {
-	m          sync.RWMutex
-	salt       string
-	expiration time.Duration
-	nonces     map[string]time.Time
-	h          hash.Hash
+	m             sync.RWMutex
+	salt          string
+	expiration    time.Duration
+	nonces        map[string]time.Time
+	h             hash.Hash
+	newExpiration chan struct{}
 }
 
+// Timeout allows you to specify how long nonces are valid for.  This function
+// is normally only called directly after creating the store, but before using
+// it.  Updating this value has the side effect of updating how often the go
+// map (which actually holds all of the nonces internally) is scanned for
+// expired nonces.
 func (s *Store) Timeout(t time.Duration) *Store {
 	s.expiration = t
+	s.newExpiration <- struct{}{}
 	return s
 }
 
+// Salt allows you to specify the salt used internally while creating nonces.
+// This should only be done after creating the store but before using it as
+// changing this value will immediately invalidate all existing nonces
+// regardless of their existence or expiration
 func (s *Store) Salt(salt string) *Store {
 	s.salt = salt
 	return s
 }
 
+// Nonce creates a nonce for the provided action.  Given the resulting string
+// and the original action string you can use *store.Verify() and *store.Peek()
+// at a later time to validate the nonce.
 func (s *Store) Nonce(action string) string {
 	s.m.Lock()
 	defer s.m.Unlock()
@@ -37,6 +76,9 @@ func (s *Store) Nonce(action string) string {
 	return h
 }
 
+// Verify validates a nonce against an action.  It checkes that all of the
+// following are true: the nonce exists, the nonce has not expired, the nonce
+// is for the action provided.
 func (s *Store) Verify(nonce, action string) bool {
 	s.m.RLock()
 	defer s.m.RUnlock()
@@ -52,6 +94,8 @@ func (s *Store) Verify(nonce, action string) bool {
 	return false
 }
 
+// Peek allows you to see if a valid matching nonce exists without actually
+// removing it from the store.
 func (s *Store) Peek(nonce, action string) bool {
 	s.m.RLock()
 	defer s.m.RUnlock()
@@ -79,17 +123,25 @@ func (s *Store) hash(action string, t time.Time) string {
 
 func (s *Store) mindExpiration() {
 	for {
-		when := <-time.After(5 * s.expiration)
-		s.m.Lock()
-		for k, v := range s.nonces {
-			if when.After(v) {
-				delete(s.nonces, k)
+		c := <-time.After(5 * s.expiration)
+		select {
+		case when := <-c:
+			s.m.Lock()
+			for k, v := range s.nonces {
+				if when.After(v) {
+					delete(s.nonces, k)
+				}
 			}
+			s.m.Unlock()
+		case <-s.newExpiration:
+			continue
 		}
-		s.m.Unlock()
 	}
 }
 
+// New returns a new nonce store.  You should always use this function instead
+// of var something = &nonce.Store{} because it sets defaults, and begins the
+// goroutine responsible for cleaning up expired nonces from the store.
 func New() (*Store, error) {
 	var randBytes = make([]byte, 20)
 	if _, e := rand.Read(randBytes); e != nil {
